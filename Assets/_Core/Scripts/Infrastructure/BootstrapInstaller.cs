9f9f3b4f977bf9e43fe7b765448b31ef8ef6fdd3
Makar Dezhemesov <Makar.Dzezhamesau@itechart-group.com>
2022-03-08 16:23:40 +0300

    Init

---
+++ Assets/_Core/Scripts/Infrastructure/BootstrapInstaller.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using Zenject;
+
+namespace ArBird.Infrastructure
+{
+    public class BootstrapInstaller : MonoInstaller
+    {
+        
+
+        public override void InstallBindings()
+        {
+
+        }
+    }
+}
---
+++ Assets/_Core/Scripts/Player/PlayerInfo.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace ArBird.Player
+{
+    public class PlayerInfo : ScriptableObject
+    {
+        [SerializeField] private GameObject _playerPrefab;
+        [SerializeField] private float _price;
+        [SerializeField] private string _name;
+        [SerializeField] private string _description;
+
+        public GameObject PlayerPrefab => _playerPrefab;
+        public float Price => _price;
+        public string Name => _name;
+        public string Description => _description;
+    }
+}
